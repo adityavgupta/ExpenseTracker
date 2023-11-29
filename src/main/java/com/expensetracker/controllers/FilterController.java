f14ed229b87af13cf3ee1098e32dda1b10664102
BrandonMelton <44936309+BrandonMelton@users.noreply.github.com>
2023-11-28 20:05:24 -0700

    Optional Filters WIP

--- src/main/java/com/expensetracker/controllers/FilterController.java
+++ src/main/java/com/expensetracker/controllers/FilterController.java
@@ -77,10 +77,22 @@ public class FilterController implements Initializable
 
     @FXML
     private void filter(ActionEvent event) throws Exception{
-        Date dateMin = java.sql.Date.valueOf(minDate.getValue());
-        Date dateMax = java.sql.Date.valueOf(maxDate.getValue());
-        double amountMin = Float.parseFloat(minAmount.getText());;
-        double amountMax = Float.parseFloat(maxAmount.getText());;
+        Date dateMin = new Date(Long.MIN_VALUE);
+        if(minDate.getValue() != null){
+            dateMin = java.sql.Date.valueOf(minDate.getValue());
+        }
+        Date dateMax = new Date(Long.MAX_VALUE);
+        if(maxDate.getValue() != null){
+            dateMax = java.sql.Date.valueOf(maxDate.getValue());
+        }
+        double amountMin = Float.NaN;
+        if(!minAmount.getText().isEmpty()){
+            amountMin = Float.parseFloat(minAmount.getText());
+        }
+        double amountMax = Float.NaN;
+        if(!maxAmount.getText().isEmpty()){
+            amountMax = Float.parseFloat(maxAmount.getText());
+        }
         String paymentMethod = paymentFilter.getText();
         String comment = commentFilter.getText();
         Boolean credit = checkCredit.isSelected();
@@ -96,11 +108,26 @@ public class FilterController implements Initializable
             String c = e.getComment();
             expenseType eType = e.getExpType();
 
-            Boolean paymentFlag = Pattern.matches(String.format(".*%s.*",paymentMethod), p);
-            Boolean commentFlag = Pattern.matches(String.format(".*%s.*",comment), c);
+            Boolean paymentFlag = true;
+            Boolean commentFlag = true;
+            Boolean minBoundary = true;
+            Boolean maxBoundary = true;
             Boolean typeFlag = (credit && eType == expenseType.Credit) || (debit && eType == expenseType.Debit);
 
-            if(a >= amountMin && a <= amountMax && paymentFlag && commentFlag && typeFlag){
+            if(!paymentMethod.isEmpty()){
+                paymentFlag = Pattern.matches(String.format(".*%s.*",paymentMethod), p);
+            }
+            if(!comment.isEmpty()){
+                commentFlag = Pattern.matches(String.format(".*%s.*",comment), c);
+            }
+            if(amountMin != Float.NaN){
+                minBoundary = a >= amountMin;
+            }
+            if(amountMax != Float.NaN){
+                maxBoundary = a <= amountMax;
+            }
+
+            if(minBoundary && maxBoundary && paymentFlag && commentFlag && typeFlag){
                 ExpenseMap.filteredMap.put(entry.getKey(),entry.getValue());
             }
         }
